8fb2068815151afbdcc5a723bd9665d86c2a3baf
Animesh Mishra <cheersanimesh@gmail.com>
2023-04-02 11:21:54 +0530

    resolved directory conflicts

--- create_dataset_dir.py
+++ create_dataset_dir.py
@@ -6,10 +6,14 @@ parrent_path= os.getcwd()
 dataset_name = vars.dataset
 
 dataset_path= os.path.join(parrent_path,"dataset_dump")
-dataset_path=os.path.join(parrent_path,"train")
+train_dataset_path=os.path.join(dataset_path,"train",dataset_name)
 
-dataset_path= os.path.join(dataset_path, dataset_name)
+test_dataset_path= os.path.join(dataset_path, "test", dataset_name)
 
-if(not os.path.exists(dataset_path)):
-    print("creating directory")
-    os.mkdir(dataset_path)
+if(not os.path.exists(train_dataset_path)):
+    print("creating training directory")
+    os.mkdir(train_dataset_path)
+
+if(not os.path.exists(test_dataset_path)):
+    print("creating test directory")
+    os.mkdir(test_dataset_path)
