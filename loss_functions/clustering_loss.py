2ae90832f7adc5867ad8766d2c61d338474d2f8b
Animesh Mishra <cheersanimesh@gmail.com>
2023-04-03 23:21:26 +0530

    cleared clustering loss

--- loss_functions/clustering_loss.py
+++ loss_functions/clustering_loss.py
@@ -30,6 +30,7 @@ class Clustering_loss:
         norm_squared= tf.reduce_sum((tf.expand_dims(embeddings,1)- cluster_centres)**2, axis=2)
         numr= 1.0 / (1.0 + (norm_squared / self.alpha))
         power = float(self.alpha + 1) / 2
+        numr= tf.math.power(numr, power)
         output = numr / tf.reduce_sum(numr, axis=1, keepdims=True)
         return output
 
@@ -40,11 +41,11 @@ class Clustering_loss:
             target1= self.get_target_distb(cluster_probs1)
             print(cluster_probs1.shape)
             print(target1.shape)
-            cluster_loss_1 = self.kl_div(tf.math.log(cluster_probs1+1e-08), target1)/cluster_probs1.shape[0]
+            cluster_loss_1 = self.kl_div(cluster_probs1, target1)/cluster_probs1.shape[0]
 
             cluster_probs2= self.get_cluster_prob(hidden2, cluster_centres=cluster_centres)
             target2= self.get_target_distb(cluster_probs2)
-            cluster_loss_2 = self.kl_div(tf.math.log(cluster_probs2+1e-08), target2)/cluster_probs2.shape[0]
+            cluster_loss_2 = self.kl_div(cluster_probs2, target2)/cluster_probs2.shape[0]
 
             return (cluster_loss_1+cluster_loss_2)/2.0
         
